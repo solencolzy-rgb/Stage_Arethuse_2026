b2e4d55d1d5e42606104e9cf0d92131d9d8565e3
solencolzy-rgb <solen.colzy@gmail.com>
2026-06-03 14:25:09 +0200

    Final

---
+++ Test/flows/test.py
@@ -0,0 +1,22 @@
+
+# This file is your entry point:
+# - add you Python files and folder inside this 'flows' folder
+# - add your imports
+# - just don't change the name of the function 'run()' nor this filename ('test.py')
+#   and everything is gonna be ok.
+#
+# Remember: everything is gonna be ok in the end: if it's not ok, it's not the end.
+# Alternatively, ask for help at https://github.com/deeplime-io/onecode/issues
+
+import onecode
+
+
+def run():
+    onecode.Logger.info(
+        """
+        #####################################################################
+        ###> Hello from Test!
+        ###> Fill in this run() function with something awesome!
+        #####################################################################
+        """
+    )
