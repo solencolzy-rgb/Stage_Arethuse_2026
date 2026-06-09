9187847c88dbcc34f602d2b7477d57bd600df2ad
solencolzy-rgb <solen.colzy@gmail.com>
2026-06-09 12:15:58 +0200

    fix: remplacement file_input par text_input pour le dossier

--- flows/test.py
+++ flows/test.py
@@ -12,7 +12,7 @@ import os
 import onecode
 import numpy as np
 import rasterio
-from onecode import Logger, Mode, Project, dropdown, file_input, text_input, file_output
+from onecode import Logger, Mode, Project, dropdown, text_input, file_output
 from rasterio.enums import Resampling
 from sklearn.decomposition import PCA
 
@@ -50,7 +50,7 @@ BAND_COMPLEXES = {
 def run():
     onecode.Logger.info(f"Hello {text_input('your name', 'OneCoder')}!")
 
-    base_path = file_input(
+    base_path = text_input(
         key="InputFolder",
         value="/path/to/landsat",
         label="Dossier bandes Landsat",
